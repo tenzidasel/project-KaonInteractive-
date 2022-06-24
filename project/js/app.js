ae7c804b59834b0aff938639dfbc451df2a99444
Tenzin Dasel <tenzidasel@brandeis.edu>
2022-06-24 12:45:19 -0400

    first commit

---
+++ project/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$("[data-js-page]").on("touchend", function(){
+		var goal_page = $(this).data("js-page");
+		$(".page").removeClass("active");
+		$(".page[data-page='"+goal_page+"']").addClass("active");
+	})
+
+})
