9958167e74663e5a3fad0c4304ad978f51f5e004
Tenzin Dasel <tenzidasel@brandeis.edu>
2022-07-15 18:05:08 -0400

    fade in transition for explore

--- project/js/app.js
+++ project/js/app.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function(){ /* executed as soon as the document is ready for DOM Manipulation. We assign
+							our function to the ready event, to tell jQuery that as soon as the document is ready, 
+							we want to call our function. */
 	
 	$("[data-js-page]").on("touchend", function(){
 		var goal_page = $(this).data("js-page");
@@ -7,5 +9,12 @@ $(document).ready(function(){
 	})
 })
 
+var div = document.querySelector(".options");
+var btn = document.querySelector(".exploreButton");
+btn.addEventListener("click", function(){
+  div.classList.add("elementToFadeInAndOut");
+  // Wait until the animation is over and then remove the class, so that
+  // the next click can re-add it.
+  setTimeout(1000);
 
-    
+});
